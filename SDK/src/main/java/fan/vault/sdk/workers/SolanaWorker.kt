d3afe469d8cc048cd96996247faa508d83eb7184
MirekR <mirek@vault.fan>
2022-10-14 09:54:27 +0100

    Change listNfts signature to match iOS sdk

--- SDK/src/main/java/fan/vault/sdk/workers/SolanaWorker.kt
+++ SDK/src/main/java/fan/vault/sdk/workers/SolanaWorker.kt
@@ -2,6 +2,7 @@ package fan.vault.sdk.workers
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 import com.metaplex.lib.Metaplex
 import com.metaplex.lib.drivers.indenty.ReadOnlyIdentityDriver
@@ -11,73 +12,70 @@ import com.metaplex.lib.solana.SolanaConnectionDriver
 import com.solana.networking.RPCEndpoint
 import fan.vault.sdk.models.JsonMetadataExt
 import fan.vault.sdk.models.NftTypes
-import fan.vault.sdk.models.NftWithArweave
+import fan.vault.sdk.models.NftWithMetadata
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.concurrent.Executors
 
 class SolanaWorker {
     private val executor = Executors.newFixedThreadPool(10)
-    private val cache = mutableMapOf<String, NftWithArweave>()
+    private val service = MoreExecutors.listeningDecorator(executor)
+    private val client = OkHttpClient()
 
-    suspend fun listNFTs(
-        walletAddress: String,
-        allowedNftTypes: List<NftTypes> = listOf(NftTypes.ALBUM)
-    ): List<NftWithArweave> {
+    suspend fun listNFTs(walletAddress: String): List<NFT> {
         val wallet = com.solana.core.PublicKey(walletAddress)
         val solanaIdentityDriver = ReadOnlyIdentityDriver(wallet, solanaConnection.solanaRPC)
         val storageDriver = OkHttpSharedStorageDriver()
         val metaplex = Metaplex(solanaConnection, solanaIdentityDriver, storageDriver)
 
-        val candidates = metaplex.nft
+        return metaplex.nft
             .findAllByOwner(wallet)
             .getOrNull()
-            ?.filterNotNull()
+            ?.filterNotNull() ?: emptyList()
+    }
 
-        val toLoad = candidates
-            ?.filterNot { cache.contains(it.mint.toBase58()) }
-
-        fetchArweaveMetadata(toLoad)
-            .forEach { cache[it.nft.mint.toBase58()] = it }
+    suspend fun listNFTsWithMetadata(
+        walletAddress: String,
+        allowedNftTypes: List<NftTypes> = listOf(NftTypes.ALBUM)
+    ): List<NftWithMetadata> {
+        val candidates = listNFTs(walletAddress)
+            .map { fetchArweaveMetadata(it) }
 
         return candidates
-            ?.mapNotNull { cache[it.mint.toBase58()] }
-            ?.filter {
+            .mapNotNull { it.get() }
+            .filter {
                 val type =
-                    it.arweave?.attributes?.firstOrNull() { it.trait_type.equals("type") }?.value
+                    it.metadata?.attributes?.firstOrNull() { it.trait_type.equals("type") }?.value
                 allowedNftTypes.contains(NftTypes.fromText(type?.toString()))
-            } ?: emptyList()
+            }
     }
 
     fun fetchArweaveMetadata(
-        nfts: List<NFT>?
-    ): List<NftWithArweave> {
-        val service = MoreExecutors.listeningDecorator(executor)
-        val client = OkHttpClient()
+        nft: NFT
+    ): ListenableFuture<NftWithMetadata?> {
+        // we're using non-standard data in the files section so nft.metadata() in not usable
+        // at the moment. Once we have fully agreed standard, we can make contribution to the metaplex library
+        return service.submit<NftWithMetadata> {
+            val request = Request.Builder()
+                .url(nft.uri)
+                .get()
+                .build()
 
-        return nfts
-            ?.map { nft ->
-                // we're using non-standard data in the files section so nft.metadata() in not usable
-                // at the moment. Once we have fully agreed standard, we can make contribution to the metaplex library
-                service.submit<NftWithArweave> {
-                    val request = Request.Builder()
-                        .url(nft.uri)
-                        .get()
-                        .build()
+            kotlin.runCatching {
+                val arweave: JsonMetadataExt? = client
+                    .newCall(request)
+                    .execute()
+                    .body?.string()
+                    ?.let { jacksonObjectMapper().readValue(it) }
 
-                    val arweave: JsonMetadataExt? = client
-                        .newCall(request)
-                        .execute()
-                        .body?.string()
-                        ?.let { jacksonObjectMapper().readValue(it) }
-
-                    NftWithArweave(
-                        nft = nft,
-                        arweave = arweave
-                    )
-                }
+                NftWithMetadata(
+                    nft = nft,
+                    metadata = arweave
+                )
             }
-            ?.mapNotNull { it.get() } ?: emptyList()
+                .onFailure { println("Failed to fetch arweave metadata for ${nft.mint.toBase58()}, it will be skipped!") }
+                .getOrNull()
+        }
     }
 
     companion object {
