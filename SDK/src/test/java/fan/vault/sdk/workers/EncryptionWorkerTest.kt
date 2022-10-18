5d31c477e352a0fa370619e11fb16677ebf22856
anthonydme <anthony@vault.fan>
2022-10-18 08:52:43 +0200

    move setup to before tag

--- SDK/src/test/java/fan/vault/sdk/workers/EncryptionWorkerTest.kt
+++ SDK/src/test/java/fan/vault/sdk/workers/EncryptionWorkerTest.kt
@@ -1,30 +1,33 @@
 package fan.vault.sdk.workers
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.junit.Assert.*
-import org.junit.Ignore
+import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 class EncryptionWorkerTest {
 
-    private val url = "https://arweave.net/zPUcS81IIrJQ0arZjBBoAyOW_bG1PSKxKXPNEL2eKa0"
-    private val client = OkHttpClient()
+    private var client: OkHttpClient? = null
+    private var url: String? = null
+
+    @Before
+    fun setup(){
+        url = "https://arweave.net/zPUcS81IIrJQ0arZjBBoAyOW_bG1PSKxKXPNEL2eKa0"
+        client = OkHttpClient()
+    }
 
     @Test
     fun `Can output a byte array`() {
         val symmetricKey = "54d61c43b9832ce5652b09404ceddfae93603004aa4684a86bf64bcbdf9e6a16"
         val request = Request.Builder()
-            .url(url)
+            .url(url!!)
             .get()
             .build()
         val data = client
-            .newCall(request)
-            .execute()
-            .body?.bytes()
+            ?.newCall(request)
+            ?.execute()
+            ?.body?.bytes()
 
         val worker = EncryptionWorker()
         val result = worker.decryptWithSymmetricKey(data!!, symmetricKey)
